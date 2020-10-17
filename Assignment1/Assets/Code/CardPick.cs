7457e266620f29d1c640bc49ce4a5f9a8f728aef
jack6412 <jack6chang@gmail.com>
2020-10-17 12:03:20 -0400

    Finish Random number test
    Just referandce test that success to give random number.

---
+++ Assignment1/Assets/Code/CardPick.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Random = UnityEngine.Random; //do random number pick
+
+public class CardPick : MonoBehaviour
+{
+    public TextMeshPro numText;
+    //Random num = new Random();
+    // Start is called before the first frame update
+    public void ButHit()
+    {
+        RandomNumber(12);
+    }
+
+    // Update is called once per frame
+    private void RandomNumber(int max)
+    {
+        int num = Random.Range(1, max+1);
+        numText.text = num.ToString();
+    }
+}
