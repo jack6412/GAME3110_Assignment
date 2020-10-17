17990029ab4212c4883f9f45549ce14df67fac8c
jack6412 <jack6chang@gmail.com>
2020-10-17 15:12:44 -0400

    add number sum and show list of number
    The valus able to do the number sum up and show the list of value.

--- Assignment1/Assets/Code/CardPick.cs
+++ Assignment1/Assets/Code/CardPick.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random; //do random number pick
@@ -8,17 +9,40 @@ using Random = UnityEngine.Random; //do random number pick
 public class CardPick : MonoBehaviour
 {
     public Text numText;
-    //Random num = new Random();
+    public Text ListText;
+
+    List<int> num1 = new List<int>();
+    int max = 1;//do for check number max of list
     // Start is called before the first frame update
     public void ButHit()
     {
-        RandomNumber(12);
+        //stor random number
+        num1.Add(RandomNumber(12));
+        
+        //show information
+        show(num1);
+        
+        //stroe the max number
+        max++;
     }
 
     // Update is called once per frame
-    private void RandomNumber(int max)
+    private int RandomNumber(int max)
     {
-        int num = Random.Range(1, max+1);
-        numText.text = num.ToString();
+        int pick = Random.Range(1, max+1);
+        return pick;
+    }
+
+    private void show(List<int> num)
+    {
+        //clear the test to empty
+        ListText.text = "";
+
+        //show the list of stroe number
+        foreach(var x in num)
+            ListText.text += x.ToString() + " ";
+
+        //show the sum up number
+        numText.text = num.Take(max).Sum().ToString();//add total number
     }
 }
