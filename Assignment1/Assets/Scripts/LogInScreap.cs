bd216a02562abbc12988a520daa906374a5360d8
jack6412 <jack6chang@gmail.com>
2020-12-07 20:27:53 -0500

    Add UI Switch
    Able switch UI from Login To Registration.
    By Press "Registration" to registration menu.
    Press "Exit" to Login menu

---
+++ Assignment1/Assets/Scripts/LogInScreap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogInScreap : MonoBehaviour
+{
+    public GameObject Registration_Scream;
+    public GameObject Login_Scream;
+
+    public void Regitsration()
+    {
+        Registration_Scream.gameObject.SetActive(true);
+        Login_Scream.gameObject.SetActive(false);
+    }
+
+    public void Login()
+    {
+        Registration_Scream.gameObject.SetActive(false);
+        Login_Scream.gameObject.SetActive(true);
+    }
+}
